82508e7d0809fb2eac0136c163d24b5f8de4f6b3
PhamNgocDuy28 <161795506+PhamNgocDuy28@users.noreply.github.com>
2024-03-12 03:26:12 +0000

    pussh code for bill payment

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect
 from carts.models import CartItem
 from store.models import Product
@@ -62,8 +62,11 @@ def payments(request):
     send_email = EmailMessage(mail_subject, message, to=[to_email])
     send_email.send()
 
-
-    return render(request, 'orders/payments.html')
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
 
 @login_required(login_url='login')  
 def place_order(request,total=0,quantity=0):
@@ -114,3 +117,29 @@ def place_order(request,total=0,quantity=0):
             return render(request, 'orders/payments.html', context)
     else:
           return redirect('checkout')
+    
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+        
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
+    
