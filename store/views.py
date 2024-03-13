e78c7926580a541e34e42f0453db769bb618fde1
PhamNgocDuy28 <161795506+PhamNgocDuy28@users.noreply.github.com>
2024-03-13 02:09:59 +0000

    Push code Submit Review User for buy product

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from orders.models import OrderProduct
 
 from store.forms import ReviewForm
 from .models import Category, ReviewRating
@@ -42,10 +43,16 @@ def product_detail(request, category_slug, product_slug):
         in_cart = CartItem.objects.filter(cart__cart_id = _cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
+
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
     
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'orderproduct': orderproduct,
     }
     return render(request, 'store/product_detail.html', context)
 
