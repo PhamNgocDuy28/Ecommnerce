23e85b84313effdb45e8e3d701bf6ae50b9e1c3a
PhamNgocDuy28 <161795506+PhamNgocDuy28@users.noreply.github.com>
2024-03-19 10:13:27 +0000

    fix profie-picture in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -16,9 +16,13 @@ class AccountAdmin(UserAdmin):
     fieldsets=()
 
 class UserProfileAdmin(admin.ModelAdmin):
-    def thumbnail(self, object):
-        return format_html('<img src="{}" width="30" style="bordere-radius:50%;">'.format(object.profile_picture.url))
+    def thumbnail(self, obj):
+        if obj.profile_picture:
+            return format_html('<img src="{}" width="30" style="border-radius:50%;">'.format(obj.profile_picture.url))
+        else:
+            return 'No image'
     thumbnail.short_description = 'Profile Picture'
-    list_display = ('thumbnail', 'user', 'city','state','country')
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
 admin.site.register(Account,AccountAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
